a19ee99728d105830e79a9e458f327c93e0c9c15
PTT154 <nguyenminhphuoc154@gmail.com>
2025-09-01 14:02:50 +0700

    su dung tailwind CLI

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                stroke: 'rgb(238 238 238)',
+            },
+        },
+        boxShadow: {
+            'solid-l': '0px 10px 120px 0px rgba(45, 74, 170, 0.1)',
+        },
+        fontSize: {
+            'hero': ['44px', { lineHeight: '58px' }],
+        },
+        borderColor: {
+            stroke: 'rgb(238 238 238)',
+        },
+    },
+    plugins: [],
+}
