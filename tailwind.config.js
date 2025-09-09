46f9d5de7f24ece97b28abdaa1d82d5299a7d048
PTT154 <nguyenminhphuoc154@gmail.com>
2025-09-10 02:23:05 +0700

    hoan thien SOLID FEATURES & Solid UI + fix lai hieu ung

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,22 @@ module.exports = {
                 'solid-l': '0px 10px 120px 0px rgba(45, 74, 170, 0.1)',
                 'solid-7': '0px 30px 50px rgba(45, 74, 170, .1)',
                 'solid-8': '0px 12px 120px rgba(45,74,170,.06)',
-                'solid-9': '0px 12px 30px rgba(45,74,170,.06)'
+                'solid-9': '0px 12px 30px rgba(45,74,170,.06)',
+                'solid-3': '0px 6px 90px rgba(8, 14, 40, .04)',
+                'solid-4': '0px 6px 90px rgba(8, 14, 40, .1)',
+                'solid-5': '0px 8px 24px rgba(45, 74, 170, .08)',
+                'solid-6': '0px 8px 24px rgba(10, 16, 35, .08)'
             },
             fontSize: {
                 'hero': ['44px', { lineHeight: '58px' }],
                 'sectiontitle3': ["44px", { lineHeight: "55px" }],
+                'sectiontitle2': ["40px", { lineHeight: "52px" }],
                 'sectiontitle': ["14px", { lineHeight: "22px" }],
                 'sectiontitle4': ["34px", { lineHeight: "48px" }],
                 'para2': ["22px", { lineHeight: "35px" }],
-                'metatitle3': ["18px", {lineHeight: "26px"}]
+                'metatitle3': ["18px", {lineHeight: "26px"}],
+                'itemtitle': ["26px", {lineHeight: "32px"}],
+                'regular': ["16px", {lineHeight: "26px"}],
             },
         },
     },
